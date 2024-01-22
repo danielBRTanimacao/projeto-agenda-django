c9d2db110b2a1776aa405b1823722929f567709f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-22 19:40:04 -0300

    Aula agenda Django models

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# Depois
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+
 class Contact(models.Model):
     ...
