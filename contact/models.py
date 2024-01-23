e6d076ae5f3fe2472cce428f332fdb286b818d74
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-23 13:37:05 -0300

    Aula projeto agenda Django Models

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
@@ -10,4 +11,9 @@ from django.db import models
 
 
 class Contact(models.Model):
-    ...
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
