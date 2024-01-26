60f247c3472c684ce919367b5c8052e859f134c5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-26 09:53:11 -0300

    customizando ações admin

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,19 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+    )
+    ordering = '-id',
+    # list_filter = 'created_date',
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = 'id', 'phone',
