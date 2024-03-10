546131398bd703d2ded8f7ce4be1fae33d5a673c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-10 15:06:16 -0300

    voltando estudar django aprofundando

--- contact/admin.py
+++ contact/admin.py
@@ -19,3 +19,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 150
     list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
