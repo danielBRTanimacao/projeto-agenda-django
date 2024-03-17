099f0f0434db81db4bb2c6da980a138c30f95d95
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-17 11:05:50 -0300

    verbose_name

--- contact/models.py
+++ contact/models.py
@@ -10,6 +10,10 @@ from django.utils import timezone
 # owner (foreign key)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
