f9321d5e0fd690fa9097d62edd392d93403c14fc
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-21 11:17:50 -0300

    entrando e criando link contato urls

--- contact/views/contacts_views.py
+++ contact/views/contacts_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from contact.models import Contact
 
 def index(request):
@@ -9,3 +10,14 @@ def index(request):
     }
 
     return render(request, 'contact/index.html', context)
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(request, 'contact/contact.html', context)
