055b11a041e506253f4ad50f10e80d8533b772bf
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-26 10:23:40 -0300

    criação de partials

--- contact/views/contacts_views.py
+++ contact/views/contacts_views.py
@@ -7,6 +7,7 @@ def index(request):
 
     context = {
         'contacts': contatcts,
+        'site_title': 'Contatos - '
     }
 
     return render(request, 'contact/index.html', context)
@@ -15,9 +16,11 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    site_title = f"{single_contact.first_name} {single_contact.last_name} - "
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(request, 'contact/contact.html', context)
