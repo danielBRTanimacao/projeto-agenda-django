cf7e74d1bb0cdb701568133466c5ff72443344a4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-04 10:57:33 -0300

    urls padrões crud e criando area 'create'

--- contact/urls.py
+++ contact/urls.py
@@ -4,7 +4,8 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('search/', views.search, name="search"),
     path('', views.index, name='index'),
+    path('search/', views.search, name="search"),
+
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
 ]
