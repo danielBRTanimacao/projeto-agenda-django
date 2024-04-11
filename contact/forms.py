ab2840c698af20df9342ad9fe286fe8c92663a6d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-11 09:27:23 -0300

    movendo o form

---
+++ contact/forms.py
@@ -0,0 +1,30 @@
+from django.core.exceptions import ValidationError
+from django import forms
+
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name', 'last_name', 'phone'
+    
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
+
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,35 +1,5 @@
 from django.shortcuts import render
-from django.core.exceptions import ValidationError
-from django import forms
-
-from contact.models import Contact
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name', 'last_name', 'phone'
-    
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
-
-
+from contact.forms import ContactForm
 
 def create(request):
     if request.method == "POST":
