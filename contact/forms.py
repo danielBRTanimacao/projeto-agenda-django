7ae35a01360ce28b6da6c60c67714623f8e2fb27
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-12 09:49:54 -0300

    validando dados form clean validation error

--- contact/forms.py
+++ contact/forms.py
@@ -36,7 +36,18 @@ class ContactForm(forms.ModelForm):
         # }
     
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            self.add_error(
+                'last_name',
+                ValidationError(
+                    'O primeiro nome não pode ser igual ao segundo!',
+                    code='invalid'
+                )
+            )
 
         self.add_error(
             'first_name',
@@ -45,13 +56,15 @@ class ContactForm(forms.ModelForm):
                 code='invalid'
             )
         )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-
         return super().clean()
+    
+    def clean_first_name(self):
+        cleaned_data = self.cleaned_data.get('first_name')
+        if cleaned_data == 'ABC':
+            self.add_error('first_name',ValidationError(
+                'Não digita abc misera',
+                code='invalid'
+            ))
+        return cleaned_data
+
 
