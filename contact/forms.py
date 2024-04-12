c29ba0ac68543319800267eafcbc599755cc0305
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-12 09:03:57 -0300

    atualizando create forms e usando widget e attrs

--- contact/forms.py
+++ contact/forms.py
@@ -4,9 +4,36 @@ from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init',
+        # })
+
     class Meta:
         model = Contact
         fields = 'first_name', 'last_name', 'phone'
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
     
     def clean(self):
         # cleaned_data = self.cleaned_data
