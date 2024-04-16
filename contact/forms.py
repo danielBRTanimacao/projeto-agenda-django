1cd78d8907d64699bfc6b298a13b6770d34bf3b8
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-16 11:32:03 -0300

    salvando form

--- contact/forms.py
+++ contact/forms.py
@@ -52,13 +52,13 @@ class ContactForm(forms.ModelForm):
                 )
             )
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro',
+        #         code='invalid'
+        #     )
+        # )
         return super().clean()
     
     def clean_first_name(self):
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 
 def create(request):
     if request.method == "POST":
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST),
+            'form': form,
         }
+
+        if form.is_valid():
+            form.save()
+            return redirect('contact:create')
+
         return render(request, 'contact/create.html', context)
         
 
