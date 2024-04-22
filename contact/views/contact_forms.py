0a3b8cdd3402914de8e52e45b14d7a2fb5cd3631
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-22 10:55:37 -0300

    atualizando contact modelForm

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from contact.forms import ContactForm
+from contact.models import Contact
 
 def create(request):
     form_action = reverse('contact:create')
@@ -26,10 +27,11 @@ def create(request):
     return render(request, 'contact/create.html', context)
 
 def update(request, contact_id):
+    contact = get_object_or_404(Contact, contact_id=contact_id, show=True)
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == "POST":
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         context = {
             'form': form,
             'form_action': form_action,
