964238c8c9c5f52020c237680be99ad48329e904
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-25 08:50:19 -0300

    update formulario contato

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -27,7 +27,7 @@ def create(request):
     return render(request, 'contact/create.html', context)
 
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, contact_id=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == "POST":
@@ -45,7 +45,7 @@ def update(request, contact_id):
         
 
     context = {
-        'form': ContactForm(),
+        'form': ContactForm(instance=contact),
         'form_action': form_action
     }
     return render(request, 'contact/create.html', context)
