760b61b76f9284500da5fc04de0dabb315cd6ad6
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-30 17:24:50 -0300

    RegisterForm

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from contact.forms import RegisterForm
 
 def register(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
     context = {
-        'form': RegisterForm()
+        'form': form
     }
     return render(request, 'contact/register.html', context)
