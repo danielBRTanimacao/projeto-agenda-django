b6fce0de4eb0b9e84185feb0c6d485c326b51834
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-02 08:42:36 -0300

    usando messagens para usuario

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from contact.forms import RegisterForm
 
 def register(request):
@@ -9,6 +10,8 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Formulario enviado com sucesso')
+            return redirect('contact:index')
 
     context = {
         'form': form
